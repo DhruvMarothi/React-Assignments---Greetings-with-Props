f32ee3e3720b320cffb96de2ab9b3315ea725e54
DhruvMarothi <51503387+DhruvMarothi@users.noreply.github.com>
2020-11-16 01:13:37 +0530

    Changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,8 @@ const Welcome = (props) => {
   return (
     <div>
       <header>
-        <h1>Hey!{props.name}</h1>
+        Hey!
+        <h1>{props.name}</h1>
       </header>
       <h2>Welcome to Newton School.</h2>
     </div>
